6e1a403345d16b543240dd07ec9215d40411b84c
M.Moaz naeem <M.Moaz naeem@MAZI>
2025-04-13 19:12:20 +0500

    Add project files.

---
+++ Task-1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variable declaration
+            // string name;
+            // String age;
+            
+            //variable initialization
+            // name = "Moaz";
+            // age = "19";
+            
+            //variable declaration & initialization
+            int age = 19;
+            string name = "Moaz";
+
+            Console.WriteLine(age);   //Printing age
+            
+            Console.WriteLine(name);   //Printing age
+        }
+    }
+}
